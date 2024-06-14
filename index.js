5e893ab28b4eec565a89eb59fdff461f2a53e834
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2024-06-14 23:56:48 +0530

    chore:comments

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-import { SchemaRegistry} from "@ethereum-attestation-service/eas-sdk";
-import { ethers } from 'ethers';
-import 'dotenv/config'
+import { SchemaRegistry } from "@ethereum-attestation-service/eas-sdk";
+import { ethers } from "ethers";
+import "dotenv/config";
 
 // Configuration constants
-const schemaRegistryContractAddress = "0x0a7E2Ff54e76B8E6659aedc9103FB21c038050D0"; // Sepolia 0.26
+const schemaRegistryContractAddress =
+  "0x0a7E2Ff54e76B8E6659aedc9103FB21c038050D0"; 
 const schemaRegistry = new SchemaRegistry(schemaRegistryContractAddress);
 
 async function registerSchema() {
-    try {
-        // Initialize provider and signer
-        const provider = new ethers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
-        const signer = new ethers.Wallet(process.env.WALLET_PRIVATE_KEY, provider);
-        schemaRegistry.connect(signer);
+  try {
+    // Initialize provider and signer
+    const provider = new ethers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
+    const signer = new ethers.Wallet(process.env.WALLET_PRIVATE_KEY, provider);
+    schemaRegistry.connect(signer);
 
-        // Initialize SchemaEncoder with the schema string
-        const schema = "string studentName, int256 age, string gender, string course, string registrationNumber, string issueDate"; 
-        const revocable = true; 
+    // Initialize SchemaEncoder with the schema string
+    const schema =
+      "string studentName, int256 age, string gender, string course, string registrationNumber, string issueDate string address";
+    const revocable = true;
 
-        const transaction = await schemaRegistry.register({
-            schema,
-            revocable,
-            // You could add a resolver field here for additional functionality
-          });
+    const transaction = await schemaRegistry.register({
+      schema,
+      revocable,
+      // You could add a resolver field here for additional functionality
+    });
 
-          
-        // Optional: Wait for transaction to be validated
-        await transaction.wait();
-        console.log("New Schema Created", transaction);
-    } catch (error) {
-        console.error("An error occurred:", error);
-    }
+    await transaction.wait();
+    console.log("New Schema Created", transaction);
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 }
 
 registerSchema();
