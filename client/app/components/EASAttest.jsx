8c34b1bff81943462e8269c724d0e2e69f15202d
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2024-06-21 23:45:57 +0530

    fix:toast msg

--- client/app/components/EASAttest.jsx
+++ client/app/components/EASAttest.jsx
@@ -3,6 +3,7 @@
 import { EAS, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
 import { ethers } from "ethers";
 import { useState } from "react";
+import {toast} from "react-hot-toast";
 
 const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e";
 
